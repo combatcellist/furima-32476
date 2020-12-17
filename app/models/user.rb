3b23d44a74181ab48304ce6e07942cad6594db94
combatcellist <combatcellist913@gmail.com>
2020-12-17 12:19:50 +0900

    コメント箇所の修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,12 +4,21 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-    validates :nickname, presence: true
-    validates :family_name, presence: true, format: {with: /\A[ぁ-んァ-ン一-龥]/ }
-    validates :first_name, presence: true, format: {with: /\A[ぁ-んァ-ン一-龥]/ }
-    validates :family_name_kana, presence: true, format: {with: /\A[ァ-ヶー－]+\z/ }
-    validates :first_name_kana, presence: true, format: {with: /\A[ァ-ヶー－]+\z/ }
-    validates :birth_date, presence: true
+    with_options presence: true do
+        validates :nickname
+        validates :birth_date
+        validates :password, length: { minimum: 6}, format: {with: /\A[a-z\d]{8,100}+\z/i }
+        
+        with_options format: {with: /\A[ぁ-んァ-ン一-龥]/ } do
+          validates :family_name 
+          validates :first_name
+        end
+
+        with_options format: {with: /\A[ァ-ヶー－]+\z/ } do
+          validates :family_name_kana
+          validates :first_name_kana
+       end
+    end
 
     has_many :items
     has_many :deals
